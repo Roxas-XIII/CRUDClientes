830a810741046f0500099e869371988842683501
Roxas-XIII <josephmora08@outlook.com>
2025-10-25 16:48:43 -0500

    Fix: Arreglados todos los conflictos (db, package, service)

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
@@ -21,34 +20,4 @@ connection.connect((err) => {
 });
 
 // Exportamos un objeto que contiene la conexión
-// para que otros archivos puedan importarlo con 'require'
 module.exports = { connection };
-=======
-const sql = require('mssql');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-    options: {
-        encrypt: false,
-        trustServerCertificate: true
-    }
-};
-
-
-const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log('✅ Conectado a SQL Server con autenticación de Windows');
-        return pool;
-    })
-    .catch(err => console.log('❌ DB Connection Error: ', err));
-
-module.exports = {
-    sql, poolPromise
-};
->>>>>>> 0346e3e42a818f84aef0c4bd7942d71f6a15a2ad
